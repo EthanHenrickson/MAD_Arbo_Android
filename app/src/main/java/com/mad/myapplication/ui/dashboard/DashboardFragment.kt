298871b8b5c54c1bdd69e7209abb346ca3470122
Ethan Henrickosn <henricksonethan@gmail.com>
2024-04-05 23:32:28 -0500

    Finished Map Layout & changed app color to green
    Finished adding all tree types,
    Filter button is now working good,
    All points are displayed on the map.

--- app/src/main/java/com/mad/myapplication/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/mad/myapplication/ui/dashboard/DashboardFragment.kt
@@ -1,25 +1,35 @@
 package com.mad.myapplication.ui.dashboard
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+import com.mad.myapplication.R
 import com.mad.myapplication.databinding.FragmentDashboardBinding
 
-class DashboardFragment : Fragment() {
+class DashboardFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
     private val dashboardViewModel: DashboardViewModel by viewModels()
 
-
+    private lateinit var mMap: GoogleMap
+    private lateinit var db: FirebaseFirestore
+    private val treelist = mutableListOf<Map<String, Any>>()
+    var TAG = "Tester"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,8 +37,20 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
+        db = Firebase.firestore
+
+        db.collection("trees").get().addOnSuccessListener { result ->
+            for (document in result) {
+                treelist.add(document.data)
+            }
+        }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
@@ -38,12 +60,82 @@ class DashboardFragment : Fragment() {
         return root
     }
 
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add additional map customizations here
+        // For example, add a marker
+
+        for (test in treelist) {
+            mMap.addMarker(MarkerOptions().position(LatLng(test.get("lat").toString().toDouble(), test.get("lng").toString().toDouble())).title(""))
+        }
+
+        val bemidji = LatLng(47.47,-94.88)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bemidji, 10F))
+
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+
+        // Asynchronously initialize the map
+        mapFragment.getMapAsync(this)
+
         binding.filterButton.setOnClickListener{
-            dashboardViewModel.onFilterClicked()
-        }}
+            mMap.clear()
+
+            val treeCheck = mutableListOf<String>()
+
+            if(binding.checkBoxAspen.isChecked){
+                treeCheck.add("aspen")
+            }
+            if(binding.checkBoxAsh.isChecked){
+                treeCheck.add("ash")
+            }
+            if(binding.checkBoxBirch.isChecked){
+                treeCheck.add("birch")
+            }
+            if(binding.checkBoxCedar.isChecked){
+                treeCheck.add("cedar")
+            }
+            if(binding.checkBoxCherry.isChecked){
+                treeCheck.add("cherry")
+            }
+            if(binding.checkBoxElm.isChecked){
+                treeCheck.add("elm")
+            }
+            if(binding.checkBoxMaple.isChecked){
+                treeCheck.add("maple")
+            }
+            if(binding.checkBoxOak.isChecked){
+                treeCheck.add("oak")
+            }
+            if(binding.checkBoxPine.isChecked){
+                treeCheck.add("pine")
+            }
+            if(binding.checkBoxSpruce.isChecked){
+                treeCheck.add("spruce")
+            }
+
+
+            for (test in treelist){
+                if (treeCheck.contains(test.get("type").toString().lowercase())) {
+                    mMap.addMarker(
+                        MarkerOptions().position(
+                            LatLng(
+                                test.get("lat").toString().toDouble(),
+                                test.get("lng").toString().toDouble()
+                            )
+                        ).title("")
+                    )
+                }
+            }
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
